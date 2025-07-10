7bea5a8f4684c6242485dd86ed80e34dfcb3f9ef
biomedlink96 <biomedlink96@gmail.com>
2025-07-10 20:47:24 +0200

    Update models.py

--- backend/db/models.py
+++ backend/db/models.py
@@ -5,9 +5,9 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    role = Column(String)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, nullable=False)
 
 class JobCard(Base):
     __tablename__ = "jobcards"
