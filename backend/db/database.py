00cb2ede912fa8f750faa68f23077788d90d100a
biomedlink96 <biomedlink96@gmail.com>
2025-07-14 22:04:50 +0200

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./biomedlink.db"  # For Render, we will update this to PostgreSQL
+DATABASE_URL = "postgresql://biomedlink_db_user:MOJnNWNkUckQ5XeyX63OESHRDVlIRJso@dpg-d1qm13c9c44c739ome00-a/biomedlink_db"  # For Render, we will update this to PostgreSQL
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
